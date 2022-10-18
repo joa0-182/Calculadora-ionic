5cd3ae2375424d6706e92d2f023b415ce94ca684
João Pedro Maria <Joaopedromaria182@gmail.com>
2022-10-17 22:21:45 -0300

    17.10

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Validators } from '@angular/forms';
+import { evaluate } from 'mathjs';
 
 @Component({
   selector: 'app-tab3',
@@ -10,22 +11,59 @@ export class Tab3Page {
 
   operacao = '';
   resultado = '';
+  numero = false;
+  caracter = true;
+  caracteres = ['.', '/', '*', '-', '+'];
 
   constructor() {}
 
   adicionarValor(valor: string){
-    this.operacao += valor;
+    this.caracter = this.caracteres.includes(valor);
+
+    if (!this.caracter) {
+      this.operacao += valor;
+      this.numero = true;
+    } else if (this.caracter && this.numero) {
+      this.operacao += valor;
+      this.numero = false;
+    }
   }
   limparOperacao(){
     this.operacao = '';
+    this.numero = false;
   }
+  interValor() {
+    //
+  }
+
+  calcularOperacao() {
+    try {
+    this.resultado = evaluate(this.operacao);
+    } catch (err) {
+      this.resultado = 'Inválido';
+    }
+  }
+
+
   limparMemoria(){
     this.operacao = '';
     this.resultado = '';
+    this.numero = false;
   }
   limparCaracter(){
     if (this.operacao.length > 0){
     this.operacao = this.operacao.substring(0, this.operacao.length -1);
-  }
+    }
+
+    const ultimo = this.operacao.substring(this.operacao.length,1);
+    this.caracter = this.caracteres.includes(ultimo);
+
+    console.log(ultimo);
+
+    if (!this.caracter) {
+      this.numero = true;
+    } else {
+      this.numero = false;
+    }
   }
 }
